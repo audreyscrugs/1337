299d83c06d94e10455495c42c523f872bbb8bfe8
Tiffany Bogus & Audrey Scrugham <student@epicodus.com>
2015-10-26 20:37:19 -0700

    Pass first test for letterE with grace and poise.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//var changers = ["e", "o", "I", "s", "S"];
+//var letterE = ["e"];
+
+var userInput = prompt("Enter in a word");
+var letterE = /e/gi;
+var newstrWebkinz = userInput.replace(letterE, '3');
+
+
+
+var leetSpeak = function(word) {
+ if (userInput === letterE); {
+   return newstrWebkinz;
+ };
+};
+
+
+
+
+  // if (word.charAt[1] = "e") {
+  //   return ("p3t");
+  // } else  {
+  //   return "not 1337"
+  // };
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe("leetSpeak", function() {
+  it("replaces the letter 'e' with the number 3", function() {
+    expect(leetSpeak("peet")).to.equal("p33t");
+  });
+
+});
